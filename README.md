9d8a77be1d6cd47eea5aaeadcdda682a70868194
Vivek Mishra <vimis@microsoft.com>
2023-10-20 12:39:35 +0530

    Adding documentation

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# AzureSubscriptionCostVisualizer
-A simplified cost view of Azure Subscriptions
+# Azure Subscription Cost Visualizer
+
+![img.png](img.png)
+
+This is a code repository for simple visualization of Azure Subscription Cost. Code can be cloned locally and with some tweaking, it can be run on local system.
+
+This provides cost details as per below:
+
+- Cost details of resources for MonthToDate.
+- Cost details of resources for last billing month
+- Cost by Resource Types.
+- Cost by Locations.
+- Cost by Resource Groups.
+- Cost by Service
+- Cost by Meter
+
+## Prerequisites
+
+In order to successfully run this solution , below prerequisites need to be available:
+
+- An Azure subscription
+- An application registered in Azure active directory (we would need client ID and client secret while running)
+- Python and flask installed in local system or environment where solution is running
+
+## Getting Started - Running the application locally
+
+Follow the step below to setup and run the solution.
+
+1. **Install python and flask to local system:**
+   - Download and install python from official [site](https://www.python.org/downloads/).
+   - Install flask by running:
+     - `pip install Flask`
+
+2. **Collect following details as they will be required to run the code** 
+   - Azure subscription ID of which cost need to be visualized
+   - Application ID 
+   - Application secret
+   - Tenant ID
+
+3. **Clone the repo locally. In main.py, replace the values collected in above step for following variables:**
+   - client_id: Replace with your application ID
+   - client_secret: Replace with your application secret
+   - tenant_id: Replace with your tenant ID
+   - subscription_id: Replace with your subscription ID
+
+4. **Running application locally:**
+
+   - Navigate to the cloned folder where 'main.py' is present
+   - Run following command to execute flask application (make sure you have replaced the above values):
+     - `python3 main.py`
+
+## Getting Started - Running the application in docker container
+
+Follow the below steps to run the application in docker container.
+
+1. ***Make sure you have docker installed in system***.
+2. Navigate to cloned folder where 'Dockerfile' exists and build image as per below command:
+   `docker build -t [IMAGE_NAME] .`
+
+   - replace [IMAGE_NAME] with your own name.
+3. Run the container using below command:
+   
+   `docker run -p 5000:5000 [IMAGE_NAME]`
+4. Open below url in browser to see the output:
+   
+   http://localhost:5000
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-client_id = ''
-client_secret = ''
-tenant_id = ''
-subscription_id = ''
+client_id = ''  #--> Replace your client ID here
+client_secret = ''  #--> Replace your client secret here
+tenant_id = ''   #--> Replace your tenant ID here
+subscription_id = ''  #--> Replace your subscription ID here
 
 token_uri = "https://login.microsoftonline.com/" + str(tenant_id) + "/oauth2/token"
 
@@ -20,38 +20,53 @@ token_response = requests.request('POST', token_uri, headers=token_req_headers, 
 access_token = json.loads(token_response.text)['access_token']
 
 costdata_uri = "https://management.azure.com/subscriptions/" + str(subscription_id) + "/providers/Microsoft" \
-                                                                                      ".CostManagement/query?api-version=2021-10-01&$top=50"
-costdata_body = '{"type":"ActualCost","dataSet":{"granularity":"None","aggregation":{"totalCost":{"name":"Cost",' \
-                '"function":"Sum"},"totalCostUSD":{"name":"CostUSD","function":"Sum"}},"sorting": [{"direction": ' \
-                '"descending","name": "Cost"}],"grouping":[{' \
-                '"type":"Dimension","name":"ResourceId"},{"type":"Dimension","name":"ResourceType"},' \
-                '{"type":"Dimension","name":"ResourceLocation"},{"type":"Dimension","name":"ChargeType"},' \
-                '{"type":"Dimension","name":"ResourceGroupName"},{"type":"Dimension","name":"PublisherType"},' \
-                '{"type":"Dimension","name":"ServiceName"},{"type":"Dimension","name":"ServiceTier"},' \
-                '{"type":"Dimension","name":"Meter"}],"include":["Tags"],"filter":{"And":[{"Dimensions":{' \
-                '"Name":"PublisherType","Operator":"In","Values":["azure"]}},{"Dimensions":{"Name":"BillingPeriod",' \
-                '"Operator":"In","Values":["202310(2023-09-18 - 2023-10-17)"]}}]}},"timeframe": "MonthToDate"}'
+                                                                                      ".CostManagement/query?api-version=2021-10-01&$top=100"
 costdata_headers = {'Authorization': 'Bearer ' +
                                      access_token, 'Content-Type': 'Application/JSON'}
 
-costdata_response = requests.request('POST', costdata_uri, headers=costdata_headers, data=costdata_body)
+costdata_monthtodate_body = '{"type":"ActualCost","dataSet":{"granularity":"None","aggregation":{"totalCost":{"name":"Cost",' \
+                            '"function":"Sum"},"totalCostUSD":{"name":"CostUSD","function":"Sum"}},"sorting":[{"direction":" ' \
+                            'descending","name":"Cost"}],"grouping":[{"type":"Dimension","name":"ResourceId"},' \
+                            '{"type":"Dimension","name":"ResourceType"},{"type":"Dimension","name":"ResourceLocation"},' \
+                            '{"type":"Dimension","name":"ChargeType"},{"type":"Dimension","name":"ResourceGroupName"},' \
+                            '{"type":"Dimension","name":"PublisherType"},{"type":"Dimension","name":"ServiceName"},' \
+                            '{"type":"Dimension","name":"ServiceTier"},{"type":"Dimension","name":"Meter"}],"include":["Tags"]}, ' \
+                            '"timeframe":"MonthToDate"}'
+costdata_lastmonth_body = '{"type":"ActualCost","dataSet":{"granularity":"None","aggregation":{"totalCost":{' \
+                          '"name":"Cost","function":"Sum"},"totalCostUSD":{"name":"CostUSD","function":"Sum"}},' \
+                          '"sorting":[{"direction":" descending","name":"Cost"}],"grouping":[{"type":"Dimension",' \
+                          '"name":"ResourceId"},{"type":"Dimension","name":"ResourceType"},{"type":"Dimension",' \
+                          '"name":"ResourceLocation"},{"type":"Dimension","name":"ChargeType"},{"type":"Dimension",' \
+                          '"name":"ResourceGroupName"},{"type":"Dimension","name":"PublisherType"},' \
+                          '{"type":"Dimension","name":"ServiceName"},{"type":"Dimension","name":"ServiceTier"},' \
+                          '{"type":"Dimension","name":"Meter"}],"include":["Tags"]}, "timeframe":"TheLastBillingMonth"}'
 
-cost_json_data = json.loads(costdata_response.text)
+costmtddata_response = requests.request('POST', costdata_uri, headers=costdata_headers, data=costdata_monthtodate_body)
+costlmddata_response = requests.request('POST', costdata_uri, headers=costdata_headers, data=costdata_lastmonth_body)
+
+costmtd_json_data = json.loads(costmtddata_response.text)
+costlmd_json_data = json.loads(costlmddata_response.text)
 
 
 @app.route('/')
 def index():
     custom_data_json = {}
     custom_data = []
-    graph_x_data = []
-    graph_y_data = []
+    mtd_x_data = []
+    mtd_y_data = []
+    lmd_x_data = []
+    lmd_y_data = []
     rstype_x_data = []
     rstype_y_data = []
     location_x_data = []
     location_y_data = []
+    service_x_data = []
+    service_y_data = []
+    meter_x_data = []
+    meter_y_data = []
     rg_x_data = []
     rg_y_data = []
-    for items in cost_json_data['properties']['rows']:
+    for items in costmtd_json_data['properties']['rows']:
         custom_data_json['cost'] = round(items[0], 2)
         custom_data_json['resourceid'] = items[2]
         custom_data_json['resource'] = (items[2]).split('/')[-1]
@@ -65,8 +80,8 @@ def index():
         custom_data_json['meter'] = items[10]
         custom_data_json['currency'] = items[12]
         custom_data.append(custom_data_json.copy())
-        graph_x_data.append((items[2]).split('/')[-1])
-        graph_y_data.append(items[0])
+        mtd_x_data.append((items[2]).split('/')[-1])
+        mtd_y_data.append(items[0])
         if items[3] not in rstype_x_data:
             rstype_x_data.append(items[3])
             rstype_y_data.append(items[0])
@@ -92,19 +107,34 @@ def index():
             new_data = old_data + items[0]
             rg_y_data[rgindex] = new_data
 
-    return render_template('index.html', costdata=custom_data, labels=graph_x_data, data=graph_y_data,
-                           glabels=rstype_x_data, gdata=rstype_y_data, llabels=location_x_data, ldata=location_y_data,rglabels=rg_x_data, rgdata=rg_y_data)
+        if items[8] not in service_x_data:
+            service_x_data.append(items[8])
+            service_y_data.append(items[0])
+        elif items[8] in service_x_data:
+            serviceindex = service_x_data.index(items[8])
+            old_data = service_y_data[serviceindex]
+            new_data = old_data + items[0]
+            service_y_data[serviceindex] = new_data
+        if items[10] not in meter_x_data:
+            meter_x_data.append(items[10])
+            meter_y_data.append(items[0])
+        elif items[10] in meter_x_data:
+            meterindex = meter_x_data.index(items[10])
+            old_data = meter_y_data[meterindex]
+            new_data = old_data + items[0]
+            meter_y_data[meterindex] = new_data
 
+    for lmitems in costlmd_json_data['properties']['rows']:
+        lmd_x_data.append((lmitems[2]).split('/')[-1])
+        lmd_y_data.append(lmitems[0])
+    print(lmd_y_data)
 
-@app.route('/examine', methods=['GET', 'POST'])
-def examine():
-    resourceid = request.form['resourceid']
-    resource_uri = "https://management.azure.com" + str(resourceid) + "?api-version=2021-04-01"
-    resource_response = requests.request("GET", resource_uri, headers=costdata_headers)
-    resource_json_data = json.loads(resource_response.text)
-
-    return render_template('kvcompliance.html', data=resource_json_data)
+    return render_template('index.html', costdata=custom_data, labels=mtd_x_data, data=mtd_y_data,
+                           glabels=rstype_x_data, gdata=rstype_y_data, llabels=location_x_data, ldata=location_y_data,
+                           rglabels=rg_x_data, rgdata=rg_y_data, lmlabels=lmd_x_data, lmdata=lmd_y_data,
+                           meterlabels=meter_x_data, meterdata=meter_y_data, servicelabels=service_x_data,
+                           servicedata=service_y_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
